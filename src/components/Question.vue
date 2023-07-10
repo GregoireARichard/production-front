78bb2865c88fa5b6d74b180efbeba9bb3b1878a7
Loris Dié <loris.die@converteo.com>
2023-07-10 15:40:47 +0200

    feat/questions view + question component

---
+++ src/components/Question.vue
@@ -0,0 +1,73 @@
+<template #main>
+  <main class="main">
+    <div class="score_container">
+      <p class="score">{{ props.user_points }}/{{ props.total_point }}</p>
+    </div>
+    <div class="block">
+      <div class="head">
+        <h4 class="head_title">{{ props.name }}</h4>
+        <p class="head_text">{{ props.description }}</p>
+        <Toasters name="clue">{{ props.clue }}</Toasters>
+        <p>Point de la question: {{ props.exercise_points }}</p>
+      </div>
+      <Button>Tester la connexion</Button>
+    </div>
+  </main>
+</template>
+<script setup lang="ts">
+interface Props {
+  responseArray?: ['response', 'index']
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  responseArray: ['test', 1],
+})
+</script>
+<style scoped lang="scss">
+.main {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100vw;
+  min-height: 100vh;
+  padding: 5rem 20rem;
+  .block {
+    position: relative;
+    padding: 5rem;
+    border-radius: 2rem;
+    background-color: #ccb4f0;
+  }
+  .head {
+    position: relative;
+    width: 100%;
+    margin-bottom: 3rem;
+
+    &_title {
+      position: relative;
+      font-size: 1.6rem;
+      font-weight: bold;
+      text-transform: uppercase;
+      margin-bottom: 2rem;
+      margin-left: 1.4rem;
+
+      &::before {
+        content: '';
+        position: absolute;
+        top: 50%;
+        left: -1.4rem;
+        transform: translate(-50%, -50%);
+        width: 1rem;
+        height: 1rem;
+        border-radius: 50%;
+        background-color: black;
+      }
+    }
+    &_text {
+      font-size: 1.6rem;
+      font-weight: normal;
+      line-height: 1.2;
+    }
+  }
+}
+</style>
---
+++ src/views/challenge/QuestionsView.vue
@@ -0,0 +1,32 @@
+<template>
+  <Layout>
+    <Question
+      name="4eme Exercice"
+      description="Vous devez créer un fichier toto à la racine de votre utilisateur avec les droits en lecture/écriture uniquement pour le groupe toto-production"
+      ,
+      clue="Pensez bien à ajouter l'utilisateur fourni au groupe toto-production"
+      user_points="10"
+      exercise_points="5"
+      total_point="20"
+    ></Question>
+  </Layout>
+</template>
+<script setup lang="ts">
+import Question from '/src/components/Question.vue'
+const api_response = {
+  next: true,
+  error: {
+    title: "Le fichier 'toto' n'existe pas",
+    message: "impossible d'accéder au fichier",
+    status_code: 404,
+  },
+  name: '4eme Exercice',
+  description:
+    'Vous devez créer un fichier toto à la racine de votre utilisateur avec les droits en lecture/écriture uniquement pour le groupe toto-production',
+  clue: "Pensez bien à ajouter l'utilisateur fourni au groupe toto-production",
+  user_points: 10,
+  exercise_points: 5,
+  total_point: 20,
+}
+</script>
+<style scoped lang="scss"></style>
